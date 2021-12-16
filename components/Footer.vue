26b49c03be57eb82e2e520066686a4d8869ac3cb
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2021-12-16 00:51:33 -0300

    feat: feature home page

---
+++ components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container d-flex justify-content-between">
+    <span class="badge badge-primary">Biblioteca Central da UFPA</span>
+    <span class="badge badge-primary">SEDEPTI</span>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <HomePage />
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html,
+body{
+  height: 100%;
+  background-image: url('@/static/img/banner.jpeg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+.btn-primary{
+  background-color: #93c645;
+}
+
+.border-primary{
+  border: #93c645;
+}
+
+.centralize{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
